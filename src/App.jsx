20b9f36f8e86350df3466cd2d2a0210df5f2cccb
Serjay <serhiinote@gmail.com>
2026-01-13 16:38:01 +0000

    React Hooks

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import StateExample from "./hooks/use-state";
+
+const App = () => {
+  return <StateExample />;
+};
+
+export default App;
---
+++ src/hooks/use-ref/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StateExample = () => {
+  return <div>StateExample</div>;
+};
+
+export default StateExample;
